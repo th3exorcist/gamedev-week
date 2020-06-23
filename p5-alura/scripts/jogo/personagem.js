b867bec70cd3611a381799904f5b30bca851b943
th3exorcist <ghostsavestheworld@gmail.com>
2020-06-23 14:38:00 -0300

    colision

--- p5-alura/scripts/jogo/personagem.js
+++ p5-alura/scripts/jogo/personagem.js
@@ -1,39 +1,35 @@
-class Personagem{
-  constructor(imagem){
+class Personagem extends Animacao {
+  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+     super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
     this.imagem = imagem;
-    this.matriz = [
-      [0, 0],
-      [220, 0],
-      [440, 0],
-      [660, 0],
-      [0, 270],
-      [220, 270],
-      [440, 270],
-      [660, 270],
-      [0, 540],
-      [220, 540],
-      [440, 540],
-      [660, 540],
-      [0, 810],
-      [220, 810],
-      [440, 810],
-      [660, 810],
-   ]
-   this.frameAtual = 0
+      
+    this.frameAtual = 0;
+    
+    this.yInicial = height - this.altura
+    this.y = this.yInicial
+    
+    this.velocidadeDoPulo = 0
+    this.gravidade = 3
+    }
+    
+  pula(){
+    this.velocidadeDoPulo = -25
   }
   
-  exibe(){  
-     image(this.imagem, 0, height - 135, 110,135,this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270);
+  aplicaGravidade() {
+  this.y = this.y + this.velocidadeDoPulo
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade 
   
-  this.anima()
+    
+    if(this.y > this.yInicial) {
+      this.y = this.yInicial
+    }
   }
   
-  anima(){
-  this.frameAtual++
-  
-  if(this.frameAtual >= this.matriz.length - 1) {
-    this.frameAtual = 0
-     
-     }
+  estaColidindo() {
+    const precisao = 0.6
+    const colisao = collideRectRect(this.x, this.y, this.largura * precisao, this.altura * precisao, inimigo.x, inimigo.y, inimigo.largura , inimigo.altura * precisao);
+    
+    return colisao;
   }
-}
+  }
